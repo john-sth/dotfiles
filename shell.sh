86d31a8c49ff492aefd9a09b473a5e23916d0e23
john <johann.spenrath@hhu.de>
2025-12-04 14:39:16 +0100

    init dotfiles

---
+++ shell.sh
@@ -0,0 +1,10 @@
+# ===================================================================================
+# shell.sh script 
+# see https://effective-shell.com/part-5-building-your-toolkit/managing-your-dotfiles
+# ===================================================================================
+#
+# If we are not running interactively do not continue loading this file.
+case $- in
+    *i*) ;;
+      *) return;;
+esac
